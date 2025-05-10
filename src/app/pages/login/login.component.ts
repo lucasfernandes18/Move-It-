8c0542a72fce2e9fb7770383f168fca6b5b01303
Seu Nome <lucasfermagalhaes@gmail.com>
2025-05-09 23:52:11 -0300

    feat: create login page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,24 +1,53 @@
 import { Component } from '@angular/core';
 import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { InputPrimaryComponent } from '../../components/imput-primary/input-primary.component';
+import { Router } from '@angular/router';
+import { LoginService } from '../../services/login.service';
+import { ToastrService } from 'ngx-toastr'; // Certifique-se que está importado corretamente
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
     DefaultLoginLayoutComponent,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    InputPrimaryComponent
   ],
+  providers: [LoginService],
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'] // também estava `styleUrl`, o correto é `styleUrls` (plural)
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
   loginForm: FormGroup;
 
-  constructor() {
+  constructor(
+    private router: Router,
+    private loginService: LoginService,
+    private toastService: ToastrService
+  ) {
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required, Validators.minLength(6)]),
     });
+  }
+
+  submit() {
+    if (this.loginForm.invalid) {
+      this.toastService.error("Formulário inválido!");
+      return;
+    }
+
+    this.loginService.login(
+      this.loginForm.value.email,
+      this.loginForm.value.password
+    ).subscribe({
+      next: () => this.toastService.success("Login feito com sucesso!"),
+      error: () => this.toastService.error("Erro inesperado! Tente novamente.")
+    });
+  }
+
+  navigate() {
+    this.router.navigate(["signup"]);
   }
 }
